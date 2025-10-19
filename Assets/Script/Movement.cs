f38a510f52df7b48a1525673b8f786da50bf221b
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2025-10-19 01:10:56 -0700

    Added follow camera and character can move/dash

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+  public float speed;
+  public float dashSpeed;
+  public float dashDuration;
+
+  private float dashTimeRemaining;
+  private bool isDashing;
+
+  private void Update()
+  {
+    float horizontal = Input.GetAxisRaw("Horizontal");
+    float vertical = Input.GetAxisRaw("Vertical");
+
+    Vector3 direction = new Vector3(horizontal, vertical);
+    if (direction.magnitude > 0)
+    {
+      direction.Normalize();
+    }
+
+    if (Input.GetKeyDown(KeyCode.Space) && !isDashing) // Dash input
+    {
+      isDashing = true;
+      dashTimeRemaining = dashDuration;
+    }
+
+    if (isDashing)
+    {
+      transform.position += direction * dashSpeed * Time.deltaTime;
+      dashTimeRemaining -= Time.deltaTime;
+
+      if (dashTimeRemaining <= 0)
+      {
+        isDashing = false;
+      }
+    }
+    else
+    {
+      transform.position += direction * speed * Time.deltaTime;
+    }
+  }
+}
