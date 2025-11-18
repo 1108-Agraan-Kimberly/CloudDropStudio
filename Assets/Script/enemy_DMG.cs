421eb217c4fd93a56fcd7a8c43f0ac76a027f636
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2025-11-17 17:02:17 -0800

    dmg

--- Assets/Script/enemy_DMG.cs
+++ Assets/Script/enemy_DMG.cs
@@ -4,7 +4,7 @@ public class enemy_DMG : MonoBehaviour
 {
     public int damage = 1;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
        collision.gameObject.GetComponent<Health>().healthState(-damage);
     }
